28f1c83a66c5a36445a8b2ff79fd19342781e3c8
imzadi-0 <harkerisabella@gmail.com>
2023-03-03 12:32:42 -0800

    updated enemy1

---
+++ Assets/Scripts/Enemy1.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(130, -100, 0);
+    }
+
+    public int health = 3;
+    public float speed = 1;
+
+    public float xmin = 130;
+    public float ymin = -100;
+    public float radius = 60;
+    public Vector3 move_direction = new Vector3 (0, 1, 0);
+
+    void Move() {
+        if (transform.position.y > (ymin + radius) && move_direction.y == 1) {
+            move_direction = new Vector3(1, 0, 0);
+        }
+        else if (transform.position.x > (xmin + radius) && move_direction.x == 1) {
+            move_direction = new Vector3(0, -1, 0);
+        }
+        else if (transform.position.y < ymin && move_direction.y == -1) {
+            move_direction = new Vector3(-1, 0, 0);
+        }
+        else if (transform.position.x < xmin && move_direction.x == -1) {
+            move_direction = new Vector3(0, 1, 0);
+        }
+        transform.position = transform.position + speed*move_direction;
+    }
+
+    void Hit() {
+        health--;
+        Debug.Log(health.ToString());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+
+        if (health==0) {
+            Destroy(this.gameObject);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D c) {
+        if (c.gameObject.name == "Player") {
+            Hit();
+            Debug.Log("hit enemy\n");
+        }
+    }
+}
+
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -12,11 +12,17 @@ public class PlayerMovement : MonoBehaviour
     public Animator anim;
 
     public int hasWeapon = 0;
+    public bool attack = false;
+
+    public float health = 5;
+
+    public bool immune = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.GetComponent<PolygonCollider2D>().enabled=false;
+        health = 5;
     }
 
     void changeSprite(Sprite newsprite) {
@@ -27,12 +33,33 @@ public class PlayerMovement : MonoBehaviour
     void Attack() {
         if (hasWeapon == 1) {
             anim.SetBool("AttackPressed", true);
+            this.GetComponent<PolygonCollider2D>().enabled=true;
+            attack = true;
+        }
+    }
+
+    void Flip() {
+        float tx = transform.localScale.x;
+        if (Input.GetAxis("Horizontal") < 0 && tx > 0) {
+            tx *= -1;
+        }
+        else if (Input.GetAxis("Horizontal") > 0 && tx < 0) {
+            tx *= -1;
+        }
+        transform.localScale = new Vector3(tx, transform.localScale.y, transform.localScale.z);
+    }
+
+    void Hit() {
+        if (!immune) {
+            health--;
+            immune = true;
         }
     }
 
     // Update is called once per frame
     void Update()
     {
+        int i = 0;
         this.GetComponent<Rigidbody2D>().velocity = new Vector2 (Input.GetAxis("Horizontal")*speed, Input.GetAxis("Vertical")*speed);
         // if (Input.GetAxis("Fire1"))
         this.GetComponent<Rigidbody2D>().rotation = 0f;
@@ -49,13 +76,29 @@ public class PlayerMovement : MonoBehaviour
 
         if (Input.GetKeyUp("space")) {
             anim.SetBool("AttackPressed", false);
+            this.GetComponent<PolygonCollider2D>().enabled=false;
+            attack = false;
+        }  
+
+        if (health == 0) {
+            Destroy(this.gameObject);
         }
+
+        Flip();
+        if (i % 10 == 0) {
+            immune = false;
+        }
+        i++;
     }
 
     void OnCollisionEnter2D(Collision2D coll) {
         this.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
         this.GetComponent<Rigidbody2D>().angularVelocity = 0f;
         // Debug.Log("collision detected\n");
+
+        if (coll.gameObject.name == "enemy1") {
+            Hit();
+        }
     }
 
     void OnTriggerEnter2D(Collider2D coll) {
@@ -63,7 +106,7 @@ public class PlayerMovement : MonoBehaviour
         if (coll.gameObject.name == "sword") {
             changeSprite(swordSprite);
             hasWeapon = 1;
+            Destroy(coll.gameObject);
         }
-        Destroy(coll.gameObject);
     }
 }
